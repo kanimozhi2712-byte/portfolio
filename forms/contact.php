ed1fa36bd4cdf2ebe88584330515edc43426f7d5
kanimozhi2712-byte <kanimozhi27121999@gmail.com>
2026-02-06 19:05:20 +0530

    file added

---
+++ forms/contact.php
@@ -0,0 +1,32 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name    = htmlspecialchars($_POST['name']);
+    $email   = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Receiving email
+    $to = "arnaldsojan@gmail.com";
+
+    // Email subject
+    $email_subject = "New Contact Message: $subject";
+
+    // Email content
+    $email_body = "From: $name\n";
+    $email_body .= "Email: $email\n\n";
+    $email_body .= "Message:\n$message\n";
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Send email
+    if (mail($to, $email_subject, $email_body, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+}
+?>
